b9bcb833a426ba832cbfc2a2d323de65b64c1c9f
Saif Bechan <saifbechan@gmail.com>
2020-01-28 20:27:42 -0300

    Toggle byte

--- src/redux/bytes/bytes.reducer.js
+++ src/redux/bytes/bytes.reducer.js
@@ -8,7 +8,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case BytesActionTypes.TOGGLE_BYTE:
       return {
-        ...state
+        ...state,
+        values: state.values.map((value, idx) =>
+          idx === action.payload ? (value ? 0 : 1) : value
+        )
       };
     default:
       return state;
