bd96c59a397fd7ab2d8255e7fe12ebe4356b1840
Saif Bechan <saifbechan@gmail.com>
2020-01-28 22:38:10 -0300

    Updates

---
+++ precache-manifest.aac5e8fdfabb255f516af8534025f860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5c3d94ebdf85ad861ca5c49e9e349f",
+    "url": "/triplebyte-spa/index.html"
+  },
+  {
+    "revision": "6088bb6d894c0b45c3c4",
+    "url": "/triplebyte-spa/static/css/main.13063441.chunk.css"
+  },
+  {
+    "revision": "c094b1da94b133dbf559",
+    "url": "/triplebyte-spa/static/js/2.7a05eb69.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/triplebyte-spa/static/js/2.7a05eb69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6088bb6d894c0b45c3c4",
+    "url": "/triplebyte-spa/static/js/main.262f0222.chunk.js"
+  },
+  {
+    "revision": "6f689590ae3a416eb042",
+    "url": "/triplebyte-spa/static/js/runtime-main.d2d69bc4.js"
+  }
+]);
